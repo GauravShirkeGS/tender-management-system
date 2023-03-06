4e084961570e16566c27593682184bf21f04246b
gauravshirkegs <shirkegaurav611@gmail.com>
2023-03-06 21:53:47 +0530

    adding Administrator model class

---
+++ src/com/tender/exceptions/AdministratorException.java
@@ -0,0 +1,13 @@
+package com.tender.exceptions;
+
+public class AdministratorException extends Exception {
+
+	public AdministratorException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public AdministratorException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+}
